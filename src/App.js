2bbecf3b0c7d4b12ff840290057c292840482f80
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2021-11-25 15:50:49 -0400

    Basis for application
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.js
    	new file:   src/componets/Box.js
    	modified:   src/index.css
    	modified:   tailwind.config.js
    
    Author
        @Edwardb11

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-
+import Example from "./componets/Box";
+import './index.css';
 function App() {
   return (
     <>
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-        <h1 className=" cursor-not-allowed inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
-          Configurando tailwind css
-        </h1>
-      </div>
+     <Example></Example>
     </>
   );
 }
