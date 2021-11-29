5859f43e4f66ea0c4db57f015388c6e7c6d31c3f
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2021-11-29 11:14:01 -0400

    Responsive
    Changes to be committed:
    	modified:   src/componets/box/Box.js
    	modified:   src/componets/button/Button.js
    
    Author
        @Edwardb11

--- src/componets/button/Button.js
+++ src/componets/button/Button.js
@@ -7,7 +7,7 @@ export function Button({  newContent, onClik }) {
       <button
         onClick={onClik}
         type="button"
-        className=" group relative md:w-1/4 w-1/4 mx-2 my-1 py-2 px-8 mb-2 justify-center  border border-transparent text-sm font-medium rounded-md text-white bg-gray-700 hover:bg-gray-300  hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        className="text-center group relative md:w-1/4 w-1/4 mx-2 my-2 py-2 px-8 mb-2 m-8 border border-transparent text-sm font-medium rounded-md text-white bg-gray-700 hover:bg-gray-300  hover:text-gray-700"
       >
         {"%" + newContent}
       </button>  );
