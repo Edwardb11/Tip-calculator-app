177b3f42621c593245c2479f380ff6157c6664f8
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2021-11-30 13:52:00 -0400

    add config colors tailwind css and add color in box and button

--- src/componets/button/Button.js
+++ src/componets/button/Button.js
@@ -7,7 +7,7 @@ export function Button({  newContent, onClik }) {
       <button
         onClick={onClik}
         type="button"
-        className="text-center group relative md:w-1/4 w-1/4 mx-2 my-2 py-2 px-8 mb-2 m-8 border border-transparent text-sm font-medium rounded-md text-white bg-gray-700 hover:bg-gray-300  hover:text-gray-700"
+        className="text-center group relative md:w-1/4 w-1/4 mx-2 my-2 py-2 px-8 mb-2 m-8  border-transparent font-black rounded-md bg-cyan-dark text-white hover:bg-cyan-strong hover:text-cyan-dark"
       >
         {"%" + newContent}
       </button>  );
