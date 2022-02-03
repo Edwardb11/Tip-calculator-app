f8a93ff7e90b6f350d1a6daf52bca92b3ab127a1
Edward Brito <edwardbrito11@hotmail.com>
2022-02-03 10:52:08 -0400

    add margin top to main select

--- src/componets/box/Box.js
+++ src/componets/box/Box.js
@@ -74,7 +74,7 @@ export default function Box() {
           tter
         </h1>
       </header>
-      <main>
+      <main className="bg-cyan-light_gray_f py-16 2xl:py-16 xl:py-16 lg:py-0 md:py-0 sm:py-0">
         <div className=" flex justify-center font-black">
           <div className="flex flex-auto flex-wrap px-7 py-7  rounded-3xl  shadow-lg bg-white  text-black     max-w-1/2  2xl:max-w-3/4 xl:max-w-3/4 lg:max-w-3/4 md:max-w-full sm:max-w-full">
             <div className="  flex-auto mx-2  max-w-1/2 2xl:max-w-1/2 xl:max-w-1/2 lg:max-w-full md:max-w-full sm:max-w-full">
